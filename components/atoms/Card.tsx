3255b671486d0394aef9c805faf284038510e201
Madeleine Hughes <70583506+maddierosecodes@users.noreply.github.com>
2025-05-07 16:33:26 +0100

    Fix card alginment issue

--- components/atoms/Card.tsx
+++ components/atoms/Card.tsx
@@ -10,7 +10,7 @@ export default function Card({ title, icon }: CardProps) {
   return (
     <div
       className="bg-mint-300 border-2 border-sage-800 p-2 sm:p-3 rounded-lg shadow-md hover:shadow-lg transition-all 
-                 flex flex-col items-center justify-start text-center hover:bg-mint-100 
+                 flex flex-col items-center justify-center text-center hover:bg-mint-100 
                  hover:-translate-y-1 duration-300 h-[150px] sm:h-[140px]"
     >
       <div className="text-sage-600 mb-1 sm:mb-2">{icon}</div>
